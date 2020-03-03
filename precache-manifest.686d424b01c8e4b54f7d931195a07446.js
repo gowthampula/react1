bb17daa570f9540dd50036cd3fe60cc24e31ac67
GowthamPula <gowtham817@gmail.com>
2020-03-04 00:36:31 +0530

    Updates

---
+++ precache-manifest.686d424b01c8e4b54f7d931195a07446.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b59605268d2acc7aa65c293e109ba5",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "f931276012758d504414",
+    "url": "/react1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2609e7bc21c494781b2c",
+    "url": "/react1/static/js/2.22fae775.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react1/static/js/2.22fae775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f931276012758d504414",
+    "url": "/react1/static/js/main.a1e9d1b1.chunk.js"
+  },
+  {
+    "revision": "8458cd5984e33d39edd8",
+    "url": "/react1/static/js/runtime-main.8ad5742a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react1/static/media/logo.5d5d9eef.svg"
+  }
+]);
